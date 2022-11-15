096feb957bea57ad7936bf0d8e17dad69a5c50d4
wayson <wayson1225@outlook.com>
2022-11-15 15:51:40 -0800

    unique query key

--- services/cache.js
+++ services/cache.js
@@ -5,5 +5,10 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function () {
   console.log('IM ABOUT TO RUN A QUERY');
 
+  // unique key for each query
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+
   return exec.apply(this, arguments);
 };
