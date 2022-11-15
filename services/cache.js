3cf3b2caa0bd8493b0b31229bf5d16281c42df97
wayson <wayson1225@outlook.com>
2022-11-15 15:54:24 -0800

    redis config

--- services/cache.js
+++ services/cache.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log('IM ABOUT TO RUN A QUERY');
 
   // unique key for each query
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  });
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
 
   return exec.apply(this, arguments);
 };
